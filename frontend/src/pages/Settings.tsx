d4f6c94c25b358803c098c57a02b6fc85e667ba8
Thanisha Belchada <thanishab77@gmail.com>
2024-02-28 18:37:23 +0530

    fix: changes in settings page

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -3,9 +3,27 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { FiLogOut } from "react-icons/fi";
 import { useState,useEffect } from "react";
-
+import axiosInstance from "@/lib/axiosInstance";
+import { useToast } from "@/components/ui/use-toast";
+import { useNavigate } from "react-router-dom";
 const Settings = () => {
-    
+    const navigate = useNavigate();
+    const {toast} = useToast();
+  async function logoutHandler(){
+    try {
+      await axiosInstance.get("/user/logout");
+      toast({
+        description:"Logged out"
+      })
+      console.log("logging out");
+      navigate("/");
+    } catch (error) {
+      toast({
+        description:"Error logging out"
+      })
+      
+    }
+  }
     const [isSmallScreen,setIsSmallScreen] = useState(window.innerWidth<=600);
 
 
@@ -22,7 +40,17 @@ const Settings = () => {
   return (
 
     <div className="md:mx-0 bg-[#F8F5CA] dark:bg-[#020817] relative mx-[1rem]">
-        <div className="w-[3rem] h-[3rem] border border-white flex justify-center items-center p- absolute top-[1.8rem] rounded-lg  right-4"> <FiLogOut size={20} /></div>
+
+
+{isSmallScreen && (
+     <button className="w-[3rem] h-[3rem] border border-white flex justify-center items-center p- absolute top-[1.8rem] rounded-lg  right-4" 
+     onClick={
+        ()=>{
+        logoutHandler();
+    }
+}><FiLogOut size={20} /></button>
+     )}
+        
     <div  className="mt-0 md:mt-[5rem] md:flex md:justify-start md:items-start flex justify-center items-center gap-0 md:gap-[15rem]  mb-[18rem]">
       {!isSmallScreen && (
          <Sidebar />
