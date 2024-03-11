fc7268617b38bbd2e2013a7c42f833f905eb2cfa
Thanisha Belchada <thanishab77@gmail.com>
2024-03-11 13:28:31 +0530

    fix: Userprofile pg

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -1,11 +1,28 @@
+// import { useEffect, useState } from "react";
+import { useState ,useEffect} from "react";
 import userimg from "/paybuddy.png";
-
-
+import axiosInstance from "@/lib/axiosInstance";
 interface UserProfileProps {
     toggle: boolean;
    
   }
-const UserProfile: React.FC<UserProfileProps> = ({toggle}) => {
+  const UserProfile: React.FC<UserProfileProps> = ({toggle}) => {
+    const [fname,setFname] = useState("Thanisha");
+    const [username,setUsername] = useState("");
+
+    useEffect(()=>{
+      async function fetchProfile(){
+        try {
+          const response = await axiosInstance.get("/account/userProfile");
+          setFname(response.data.firstName);
+          setUsername(response.data.username);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      fetchProfile();
+    },[])
+
 
   return (
     <div
@@ -15,12 +32,14 @@ const UserProfile: React.FC<UserProfileProps> = ({toggle}) => {
           : "bg-transparent rounded-xl p-2"
       }`}
     >
-      <div className="min-w-[3.5rem] h-[3.5rem]">
-        <img
+      <div className="min-w-[3.5rem] h-[3.5rem] bg-white  px-auto flex justify-center items-center rounded-full">
+       <div className="text-black text-3xl"> {fname.slice(0,1)}</div>
+        {/* <img
           src={userimg}
           alt="user-img"
           className="md:w-[3.5rem] md:h-[3.5rem] w-[2rem] rounded-full object-cover"
-        />
+        /> */}
+
       </div>
 
       {/* <div className={`${props.toggle ? "text-black" : "text-white"}`}
@@ -28,10 +47,10 @@ const UserProfile: React.FC<UserProfileProps> = ({toggle}) => {
       <div className={toggle ? "opacity-0 delay-200" : "text-white"}>
         <h3 className="text-xl">
           
-            <p className="username font-Lilita text-xl">name</p>
+            <p className="username font-Lilita text-xl">{fname}</p>
        
         </h3>
-        <span className="text-[0.75rem] opacity-60 ">email</span>
+        <span className="text-[0.75rem] opacity-60 ">{username}</span>
       </div>
     </div>
   );
