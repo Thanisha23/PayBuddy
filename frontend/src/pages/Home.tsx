01638824cd0387571ddfde63a446d7d84cc28a43
Thanisha Belchada <thanishab77@gmail.com>
2024-03-18 23:36:33 +0530

    fix: UI of Home pg

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,67 +1,91 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { CiHome } from "react-icons/ci";
 import { CiViewBoard } from "react-icons/ci";
 import { CiUser } from "react-icons/ci";
 const Home = () => {
   return (
     
-  //  <div className="bg-[#CECDB7] h-screen w-[100] relative">
-   <div className="bg-[white] mx-[2rem]  dark:bg-[#020817] h-screen w-[100] relative font-roboto">
-    <div className=" md:w-[4rem] dark:bg-white dark:rounded-full dark:h-[4rem] w-[8rem] md:left-34 md:h-[4rem] h-[8rem]  absolute md:left-10 md:top-9 top-56 left-0 right-0 md:mx-0 mx-auto pt-1">
-
-
-<img src="./finalLogo.jpeg" alt="" />
-    </div>
-    
-    <div className="md:w-full gap-[14rem] flex justify-center items-center md:pt-[4.5rem] pt-[4rem] md:pl-[25rem]">
-      <div className={`border border-[#32374A] dark:border-white font-normal text-xs  md:py-1 md:pt-2 rounded-full py-[0.5rem] flex justify-center items-center gap-7 md:gap-11 px-8 text-[#32374A] dark:text-white`}>
-        <div>
-        <h2 className="flex justify-center items-center"><CiHome /></h2>
-          <h1 className="pb-1 pt-1 hover:underline">Home</h1>
-        </div>
-        <div><h2 className="flex justify-center items-center"><CiViewBoard /></h2>
-          <h1 className="pb-1 pt-1  hover:underline">My Dashboard</h1></div>
-        <div><h2 className="flex justify-center items-center"><CiUser /></h2>
-        <h1 className="pb-1 pt-1  hover:underline">My Profile</h1>
-        </div>
-        
-
+    <div className="bg-[white] mx-[2rem] md:mx-[4rem]  dark:bg-[#020817] h-screen w-[100] relative font-roboto">
+      {/* logo */}
+      <div className="dark:rounded-full dark:h-[4rem] w-[4rem] md:left-34 h-[8rem] md:rounded-full absolute md:left-10 md:top-9 top-56 left-0 right-0 md:pt-[2.2rem] md:mx-0 mx-auto pt-1 md:block hidden">
+        <img
+          className="rounded-full md:w-[3rem] md:h-[3rem]"
+          src="./finalLogo.jpeg"
+          alt=""
+        />
       </div>
-   <div className="md:flex md:justify-center md:items-center md:gap-5 md:pl-8 hidden">
-<a href="/signup"><Button variant="custom">Signup</Button>
-</a>
-<a href="/signin"><Button variant="custom">Signin</Button>
-</a>
-</div>
-   
+
+      {/* navbar */}
+      <div className="md:w-full gap-[14rem] flex justify-center items-center md:pt-[4.5rem] pt-[4rem] md:pl-[22rem]">
+        <div
+          className={`border border-[#32374A] dark:border-white font-normal text-xs  md:py-1 md:pt-2 rounded-full py-[0.5rem] flex justify-center items-center gap-7 md:gap-11 px-8 text-[#32374A] dark:text-white`}
+        >
+          <div>
+            <h2 className="flex justify-center items-center">
+              <CiHome size={18} />
+            </h2>
+            <h1 className="pt-1 hover:underline">Home</h1>
+          </div>
+          <div>
+            <h2 className="flex justify-center items-center">
+              <CiViewBoard size={18} />
+            </h2>
+            <h1 className="pt-1  hover:underline">Dashboard</h1>
+          </div>
+          <div>
+            <h2 className="flex justify-center items-center">
+              <CiUser size={18} />
+            </h2>
+            <h1 className=" pt-1  hover:underline">Profile</h1>
+          </div>
+        </div>
+        {/* buttons */}
+        <div className="md:flex md:justify-center md:items-center md:gap-5 md:pl-8 hidden">
+          <a href="/signup">
+            <Button variant="custom">Signup</Button>
+          </a>
+          <a href="/signin">
+            <Button variant="custom">Signin</Button>
+          </a>
+        </div>
+      </div>
+     
+      <div className="bg-white md:mt-[5rem] mt-[5rem] md:flex md:justify-center md:items-center md:gap-[11rem] sm:gap-[3rem] gap-5 px-[1rem] sm:px-[1rem] rounded-2xl border border-[#32374A] md:border-transparent pt-6">
+        <div className="block md:hidden w-full mb-4"><img
+          className="rounded-full w-[6rem] h-[6rem] mx-auto"
+          src="./finalLogo.jpeg"
+          alt=""
+        /></div>
+        <div className="flex flex-col justify-start items-center">
+          <p className="text-[#32374A] text-2xl md:text-5xl md:p-[1rem] font-medium md:px-8 md:pb-0 pb-4 md:mt-0 text-center md:text-left font-rakkas relative">
+            <p>Simplify Payments,</p>
+            <p> Elevate Experiences with</p>
+            <p className="mb-6">PayBuddy.</p>
+            <a className="" href="/signup">
+              <Button variant="newVariant">Get Started</Button>
+            </a>
+          </p>
+          {/* <div className="absolute bottom-[14.5rem] left-[13.8rem]"></div> */}
+        </div>
+        <div className="hidden md:block">
+          <img
+            className="md:h-[30rem]  md:w-[28rem]"
+            src="/home-pg-1.jpg"
+            alt="photo hai"
+          />
+        </div>
+        {/* </div> */}
+      </div>
+      <div className="flex justify-center items-center gap-[4rem] md:hidden mt-11 font-rakkas">
+        <a href="/signup">
+          <Button variant="custom">Signup</Button>
+        </a>
+        <a href="/signin">
+          <Button variant="custom">Signin</Button>
+        </a>
+      </div>
     </div>
-      {/* <div className="bg-[#B6AA9D] w-[100] h-screen">  */}
-    <div className="bg-white md:mt-[5rem] mt-[5rem] flex justify-center items-center md:gap-[11rem] sm:gap-[3rem] gap-5 px-[1rem] sm:px-[1rem] rounded-2xl border border-[#32374A] md:border-transparent"> 
+  );
+};
 
-
-        
-     {/* <div className="flex flex-col gap-7 justify-center items-center my-[17rem]"> */}
-
-        {/* <a href="/signup"><Button variant="outline">Signup</Button>
-</a>
-<a href="/signin"><Button variant="outline">Signin</Button>
-</a> */}
-<div className="flex flex-col justify-start items-center"><p className="text-[#32374A] text-2xl md:text-5xl md:p-[1rem] font-medium md:px-8 md:pb-0 pb-4 md:mt-0 mt-40 text-center md:text-left font-rakkas relative"><p>Simplify Payments,</p><p> Elevate Experiences with</p> <p className="mb-6">PayBuddy.</p>
-<a className="" href="/signup"><Button variant="newVariant">Get Started</Button>
-</a></p>
-{/* <div className="absolute bottom-[14.5rem] left-[13.8rem]"></div> */}
-</div>
-<div className="hidden md:block"><img className="md:h-[30rem]  md:w-[28rem]" src="/home-pg-1.jpg" alt="photo hai" /></div>
-    {/* </div> */}
-    </div>
-    <div className="flex justify-center items-center gap-[4rem] md:hidden mt-11 font-rakkas">
-<a href="/signup"><Button variant="custom">Signup</Button>
-</a>
-<a href="/signin"><Button variant="custom">Signin</Button>
-</a>
-</div>
-   </div>
-  )
-}
-
-export default Home
+export default Home;
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,7 +1,7 @@
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/ui/button"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -9,11 +9,11 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useToast } from "@/components/ui/use-toast"
-import { useNavigate } from "react-router-dom"
-import axiosInstance from "@/lib/axiosInstance"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "@/lib/axiosInstance";
 const formSchema = z.object({
   username: z.string().min(3, {
     message: "Invalid email",
@@ -21,108 +21,116 @@ const formSchema = z.object({
   password: z.string().min(6, {
     message: "Invalid Password",
   }),
-})
+});
 
 const Signin = () => {
   const navigate = useNavigate();
-const {toast} = useToast();
-const form = useForm<z.infer<typeof formSchema>>({
-  resolver: zodResolver(formSchema),
-  defaultValues: {
-    username: "",
-    password:"",
-  },
-})
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
-async function onSubmit(values: z.infer<typeof formSchema>) {
-try {
-  toast({
-    description:"Loading...",
-   
-  })
-    const response = await axiosInstance.post(
-      "/user/signin",
-      values,
-      {
-        headers:{
-          "Content-Type":"application/json",
-        },
-        withCredentials:true,
-      }
-    )
-    if(response.status === 200){
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
       toast({
-        description:"Login successful"
+        description: "Loading...",
       });
-      navigate("/dashboard")
-    }else{
+      const response = await axiosInstance.post("/user/signin", values, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        toast({
+          description: "Login successful",
+        });
+        navigate("/dashboard");
+      } else {
+        toast({
+          description: "Login Failed",
+        });
+      }
+    } catch (error) {
       toast({
-        description:"Login Failed"
-      })
+        description: "Login Error",
+      });
+      console.log(error);
     }
-} catch (error) {
-  toast({
-    description:"Login Error"
-  
-})
-console.log(error);
-}
-  // console.log(values)
-}
+    // console.log(values)
+  }
   return (
-    
-      <div className="my-[3rem] md:my-[5rem] mx-auto  md:w-[70%] w-[85%] border md:border-zinc-50/30  border-x-transparent border-y-transparent  rounded-xl md:flex md:justify-center md:items-center ">
-        <div  className="md:flex md:justify-center md:items-center md:w-[50%] w-[100%] bg-white rounded-t-full md:rounded-sm">
-    <img className="mx-auto ml-3 md:h-[35rem]" src="/logo.png" alt="logo" />
-    
-    </div>
-        <div className="md:py-[1rem] py-[4rem] md:pl-[0.8rem] md:w-[43%] w-[100%] mx-auto border md:border-transparent border-zinc-50/30 relative">
-       
-            <h1 className="font-medium md:font-bold md:text-xl md:w-[14rem] w-[8rem] text-left leading-6 absolute md: md:left-1 bottom-[21rem] left-4">Welcome back to PayBuddy!</h1>
-           <div className="leading-6 absolute md:bottom-[20.8rem] md:right-0 right-4 md: bottom-[20.3rem]"> <h2 className="text-[0.8rem] text-gray-300/50 text-center md:w-[12rem] w-[6rem] leading-4">Don't have an account?</h2>
-            <h2 className="font-medium text-xs text-center pt-2"><a href="/signup">Signup here</a></h2></div>
-          
-      <div className="md:px-0 px-[1.5rem] md:pt-0 pt-8"><Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="Enter your email" {...field} />
-              </FormControl>
-            
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-          <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="Enter your password" {...field} />
-              </FormControl>
-              
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-         
-        <Button variant={"outline"} type="submit" disabled={form.formState.isSubmitting}>{form.formState.isSubmitting ? "Submitting..." : "Submit"}</Button>
-       
-      </form>
-     
-    </Form></div>
-    </div>
-  
-    
-    </div>
+    <div className="my-[3rem] md:my-[5rem] mx-auto  md:w-[70%] w-[85%] border md:border-zinc-50/30  border-x-transparent border-y-transparent  rounded-xl md:flex md:justify-center md:items-center ">
+      <div className="lg:flex lg:justify-center lg:items-center lg:w-[50%] md:hidden w-[100%] bg-white rounded-t-full md:rounded-sm">
+        <img className="mx-auto ml-3 md:h-[35rem]" src="/logo.png" alt="logo" />
+      </div>
+      <div className="md:py-[1rem] py-[4rem] md:pl-[0.8rem] md:w-[43%] w-[100%] mx-auto border md:border-transparent border-zinc-50/30">
+        <div className="flex justify-center items-center">
+          <h1 className="font-medium md:font-bold md:text-xl md:w-[14rem] w-[8rem] text-left leading-6 ">
+            Welcome back to PayBuddy!
+          </h1>
+        </div>
 
-  )
-}
+        <div className="md:px-0 px-[1.5rem] md:pt-0 pt-8">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your email" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        placeholder="Enter your password"
+                        {...field}
+                      />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                variant={"outline"}
+                type="submit"
+                disabled={form.formState.isSubmitting}
+              >
+                {form.formState.isSubmitting ? "Submitting..." : "Submit"}
+              </Button>
+              <div className="leading-6 flex justify-center items-center">
+                <h2 className="text-[0.8rem] text-gray-300/50 md:w-[12rem] w-[6rem] leading-4">
+                  Don't have an account?
+                </h2>
+                <h2 className="font-medium text-xs">
+                  <a href="/signup">Signup here</a>
+                </h2>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Signin;
