c2869c8c667a992c45e06d20b80e45c0c9b36a17
Thanisha Belchada <thanishab77@gmail.com>
2024-03-19 09:36:59 +0530

    fix: UI of Home pg

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -84,11 +84,14 @@ const form = useForm<z.infer<typeof formSchema>>({
   
 }
   return (
-    <div className="font-roboto md:my-[4rem] my-[3rem] mx-auto  md:w-[73%] border md:border-zinc-50/30  border-x-transparent border-y-transparent rounded-lg md:flex md:justify-center md:items-center">
-       <div  className="md:flex md:justify-center md:items-center md:w-[50%] md:mx-0 mx-auto w-[80%] bg-white rounded-t-full md:rounded-sm  ">
-    <img className="w-full md:h-[38rem] pl-8" src="/logo.png" alt="logo" />
+    <div className="font-roboto md:my-[4rem] my-[1rem] mb-[5rem] h-[15rem] md:h-[40rem] mx-auto  md:w-[73%] border md:border-zinc-50/30  border-x-transparent border-y-transparent rounded-lg md:flex md:justify-center md:items-center w-[100%]">
+       {/* <div  className="md:w-[50%] md:mx-0 mx-auto w-[80%] bg-white rounded-t-full md:rounded-sm h-[100%]">
+    <img className="w-full h-[80%] pl-8" src="/logo.png" alt="logo" />
+    </div> */}
+    <div className="md:w-[50%] w-[90%] rounded-t-3xl md:rounded-sm bg-white md:h-full h-[60%]  flex justify-center items-center mx-auto md:mx-0">
+    <img src="/logo-final.png" className="md:w-[26rem] md:h-[26rem] w-[7rem] h-[7rem]" alt="" />
     </div>
-      <div className="py-[1rem] md:pl-[0.8rem] md:w-[43%] w-[80%] mx-auto border md:border-transparent border-zinc-50/30 ">
+      <div className="py-[1rem] md:pl-[0.8rem] md:w-[43%] md:h-full h-[239%] w-[90%] mx-auto border md:border-transparent border-zinc-50/30 relative">
 
       <div className="">
             <h1 className="font-medium md:text-2xl text-xl  text-center leading-6 font-rakkas">Signup to PayBuddy</h1>
@@ -153,8 +156,8 @@ const form = useForm<z.infer<typeof formSchema>>({
             </FormItem>
           )}
         />
-        <Button variant={"outline"} type="submit" disabled={form.formState.isSubmitting}>{form.formState.isSubmitting ? "Submitting..." : "Submit"}</Button>
-        <div className=""> <h2><span className="text-[0.8rem] text-gray-300/50">Already a member?</span></h2>
+        <Button className="" variant={"outline"} type="submit" disabled={form.formState.isSubmitting}>{form.formState.isSubmitting ? "Submitting..." : "Submit"}</Button>
+        <div className="absolute bottom-4 md:bottom-20"><h2><span className="text-[0.8rem] text-gray-300/50">Already a member?</span></h2>
             <h2 className="font-medium text-xs"><a href="/signin">Log in here</a></h2></div>
       </form>
     </Form>
