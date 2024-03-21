53a1f88aee505128f57d6e26720589da4529ffc2
Thanisha Belchada <thanishab77@gmail.com>
2024-03-22 00:28:51 +0530

    fix: minor changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
-import { createContext } from "react";
+import { createContext,useEffect } from "react";
 
 export const Context = createContext<{
   isAuthenticated: boolean;
@@ -12,7 +12,12 @@ export const Context = createContext<{
 }>({ isAuthenticated: false, setIsAuthenticated: () => {} });
 
 const AppWrapper = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    localStorage.getItem('isAuthenticated') === 'true'
+  );
+  useEffect(() => {
+    localStorage.setItem('isAuthenticated', isAuthenticated.toString());
+  }, [isAuthenticated]);
 
   return (
     <Context.Provider value={{ isAuthenticated, setIsAuthenticated }}>
