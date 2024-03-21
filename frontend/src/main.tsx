0e3c4fbd0c587fc56210270128b6872198a3dae7
Thanisha Belchada <thanishab77@gmail.com>
2024-03-21 23:18:32 +0530

    fix: Changes in vercel.json

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,42 +1,32 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Toaster } from "@/components/ui/toaster"
-import { ThemeProvider } from "@/components/theme-provider.tsx"
-import { createContext } from 'react'
-
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { createContext } from "react";
 
 export const Context = createContext<{
   isAuthenticated: boolean;
   setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
 }>({ isAuthenticated: false, setIsAuthenticated: () => {} });
 
-
 const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
- const [isAuthenticated,setIsAuthenticated] = useState(false);
-
-return (
-    <Context.Provider value={{isAuthenticated,setIsAuthenticated}}>
-<App />
+  return (
+    <Context.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      <App />
     </Context.Provider>
-);
+  );
 };
 
-
-
-
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark"  storageKey="vite-ui-theme">
-  
-<AppWrapper />
-   
-    <Toaster />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AppWrapper />
+
+      <Toaster />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
